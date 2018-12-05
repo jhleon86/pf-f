2284f1548083983075bea723c1b0666b157c3ff3
DESKTOP-5533OO2\jhleon <jhleon869@gmail.com>
2018-12-05 20:13:26 +0900

    first commit

---
+++ src/main/java/com/plafoo/front/PlafooFrontApplication.java
@@ -0,0 +1,12 @@
+package com.plafoo.front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlafooFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlafooFrontApplication.class, args);
+	}
+}
