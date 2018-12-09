dde5d83d4223f3acaf940ab7fcbf7bc22a62a94b
DESKTOP-5533OO2\jhleon <jhleon869@gmail.com>
2018-12-09 14:02:27 +0900

    real-application.yml 경로병경

--- src/main/java/com/plafoo/front/PlafooFrontApplication.java
+++ src/main/java/com/plafoo/front/PlafooFrontApplication.java
@@ -11,8 +11,7 @@ public class PlafooFrontApplication {
 	public static final String APPLICATION_LOCATIONS = "spring.config.location="
 			+ "classpath:application.yml,"
 			+ "C:\\workspace_sts\\real\\real-application.yml," /*로컬(windows)*/
-			+ "/home/ec2-user/app/config/plafoo-front/real-application.yml"; /*EC2*/
-	
+			+ "/app/config/plafoo-front/real-application.yml"; /*EC2*/
 
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(PlafooFrontApplication.class)
