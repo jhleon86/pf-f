e0b8aaf6676af08f933b8bb5fb8e1d9fc1541289
DESKTOP-5533OO2\jhleon <jhleon869@gmail.com>
2018-12-17 19:19:20 +0900

    intercom app

--- src/main/java/com/plafoo/front/web/MsgAppController.java
+++ src/main/java/com/plafoo/front/web/MsgAppController.java
@@ -14,13 +14,13 @@ public class MsgAppController {
  
     	StringBuilder sb = new StringBuilder();
 
-    	sb.append("canvas: {");
+    	sb.append("{canvas: {");
     	sb.append("content: {");
     	sb.append("components: [");
     	sb.append("{ type: \"button\", label: \"Check your location(map)\", style: \"primary\", id: \"url_button\", action: {type: \"sheet\", url : \"https://plafoo.com/\"} }");
     	sb.append("]");
     	sb.append("}");
-    	sb.append("}"); 
+    	sb.append("}}"); 
     	
     	return sb.toString();
     }
@@ -30,13 +30,13 @@ public class MsgAppController {
  
     	StringBuilder sb = new StringBuilder();
 
-    	sb.append("canvas: {");
+    	sb.append("{canvas: {");
     	sb.append("content: {");
     	sb.append("components: [");
     	sb.append("{ type: \"text\", text: \"Someone just clicked something AND you just created a new component!\", style: \"header\", align: \"center\" }");
     	sb.append("]");
     	sb.append("}");
-    	sb.append("}");
+    	sb.append("}}");
     	
     	 
     	
