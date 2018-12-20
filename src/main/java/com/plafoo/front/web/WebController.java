35eb3b126094a684a6328e45c906ba4f215c7649
DESKTOP-5533OO2\jhleon <jhleon869@gmail.com>
2018-12-20 21:08:03 +0900

    no message

--- src/main/java/com/plafoo/front/web/WebController.java
+++ src/main/java/com/plafoo/front/web/WebController.java
@@ -3,8 +3,6 @@ package com.plafoo.front.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import com.plafoo.front.service.PostsService;
 
@@ -38,7 +36,7 @@ public class WebController {
 //    }
     
     
-    @PostMapping("/map")
+    @GetMapping("/map")
     public String map() { 
     	return "map";
     }
